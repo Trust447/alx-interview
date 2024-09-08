3ace6802c9f83435c5a27c7002daf6e3642b574b
trust <isaactrust447@gmail.com>
2024-09-08 21:51:00 +0100

    go

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python3
-'''A module for working with lockboxes.
-'''
+""" Module for solving prime game question """
 
 
-def canUnlockAll(boxes):
-    '''Checks if all the boxes in a list of boxes containing the keys
-    (indices) to other boxes can be unlocked given that the first
-    box is unlocked.
-    '''
-    n = len(boxes)
-    seen_boxes = set([0])
-    unseen_boxes = set(boxes[0]).difference(set([0]))
-    while len(unseen_boxes) > 0:
-        boxIdx = unseen_boxes.pop()
-        if not boxIdx or boxIdx >= n or boxIdx < 0:
+def isWinner(x, nums):
+    """function that checks for the winner"""
+    if not nums or x < 1:
+        return None
+
+    max_num = max(nums)
+
+    my_filter = [True for _ in range(max(max_num + 1, 2))]
+
+    for i in range(2, int(pow(max_num, 0.5)) + 1):
+        if not my_filter[i]:
             continue
-        if boxIdx not in seen_boxes:
-            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
-            seen_boxes.add(boxIdx)
-    return n == len(seen_boxes)
+        for j in range(i * i, max_num + 1, i):
+            my_filter[j] = False
+
+    my_filter[0] = my_filter[1] = False
+    y = 0
+
+    for i in range(len(my_filter)):
+        if my_filter[i]:
+            y += 1
+        my_filter[i] = y
+
+    player1 = 0
+
+    for x in nums:
+        player1 += my_filter[x] % 2 == 1
+
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 > len(nums):
+        return "Maria"
+    
+    return "Ben"
